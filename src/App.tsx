a00026a1ad0d24c9bd0e836b782a9a951dfa61d6
Marcelo Andrade <marcelo.andrade@itsector.pt>
2022-04-28 18:08:01 +0100

    [ITW] - Added Some Company References

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <img
+        src={
+          "https://content.itsector.pt/hs-fs/hubfs/Website%20Resources/ITSector_Logo.png"
+        }
+        className="company-logo"
+        alt="company-logo"
+        style={{
+          position: "absolute",
+          width: "200px",
+          height: "56px",
+          left: 0,
+          padding: "24px",
+        }}
+      />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src={"https://content.itsector.pt/hubfs/Tech-OClock-Logo-V1.png"}
+          className="App-logo"
+          alt="logo"
+        />
+        <p>ITSector Tech O'Clock Webinar</p>
+        Integração React em múltipla plataforma
       </header>
     </div>
   );
