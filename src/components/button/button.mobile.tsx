2479fa2003497fa64f02fc6fb5d82b934d0f063e
Marcelo Andrade <marcelo.andrade@itsector.pt>
2022-05-27 11:26:16 +0100

    fix: mobile buttons

--- src/components/button/button.mobile.tsx
+++ src/components/button/button.mobile.tsx
@@ -6,5 +6,25 @@ export const ButtonMobile: React.FC<ButtonProps> = ({
   text,
   style,
 }) => {
-  return <></>;
+  return (
+    <button
+      style={{
+        display: "inline-block",
+        padding: "0.3em 1.2em",
+        margin: "0 0.3em 0.3em 0",
+        borderRadius: "2em",
+        boxSizing: "border-box",
+        textDecoration: "none",
+        fontWeight: 300,
+        color: "#FFFFFF",
+        backgroundColor: "blue",
+        textAlign: "center",
+        transition: "all 0.2s",
+        ...style,
+      }}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
 };
